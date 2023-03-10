bf2846489f00e539327a0feb4d490d256fe43145
PLR <51248199+plredmond@users.noreply.github.com>
2023-03-10 15:53:41 -0800

    nix: narrowly specify depdency overridese

--- default.nix
+++ default.nix
@@ -1,15 +1,19 @@
-{ config ? { allowBroken = true; }
+{ config ? { }
 , pkgs ? import <nixpkgs> { inherit config; }
 , shell ? false
 }:
 
 let
   ghc = "ghc810";
+  haskellPackages = pkgs.haskell.packages.${ghc};
+  withNativeInputs = pkgs: d: d.overrideAttrs (old: { nativeBuildInputs = old.nativeBuildInputs ++ pkgs; });
   src = pkgs.nix-gitignore.gitignoreSource [ "*.cabal" "dist/" "*.nix" "result" ] ./.;
-  drv = pkgs.haskell.packages.${ghc}.callCabal2nix "mog" src { };
+  drv = haskellPackages.callCabal2nix "mog" src {
+    gitlib-libgit2 = pkgs.haskell.lib.markUnbroken haskellPackages.gitlib-libgit2;
+  };
   env = drv.envFunc { withHoogle = true; };
 in
 
 if shell
-then env
+then withNativeInputs [ pkgs.ghcid ] env # runhaskell Setup.hs configure && ghcid -c 'runhaskell Setup.hs repl'
 else drv
