6f15305d10fbb0dcf2e89a1eb0729882ca41f868
PLR <51248199+plredmond@users.noreply.github.com>
2023-03-10 15:56:06 -0800

    nix: add commands to remind myself about how to start things; TODO: use bash aliases

--- default.nix
+++ default.nix
@@ -4,9 +4,11 @@
 }:
 
 let
+  # setup & util
   ghc = "ghc810";
   haskellPackages = pkgs.haskell.packages.${ghc};
   withNativeInputs = pkgs: d: d.overrideAttrs (old: { nativeBuildInputs = old.nativeBuildInputs ++ pkgs; });
+  # package
   src = pkgs.nix-gitignore.gitignoreSource [ "*.cabal" "dist/" "*.nix" "result" ] ./.;
   drv = haskellPackages.callCabal2nix "mog" src {
     gitlib-libgit2 = pkgs.haskell.lib.markUnbroken haskellPackages.gitlib-libgit2;
@@ -15,5 +17,8 @@ let
 in
 
 if shell
-then withNativeInputs [ pkgs.ghcid ] env # runhaskell Setup.hs configure && ghcid -c 'runhaskell Setup.hs repl'
+then withNativeInputs [ pkgs.ghcid ] env
+# nix-shell --run "runhaskell Setup.hs configure && runhaskell Setup.hs repl"
+# nix-shell --run "runhaskell Setup.hs configure && ghcid -c 'runhaskell Setup.hs repl'"
+
 else drv
