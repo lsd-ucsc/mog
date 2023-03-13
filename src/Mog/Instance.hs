43f726d4bb71d6505b6a543c0ae88486bdd67c6a
PLR <51248199+plredmond@users.noreply.github.com>
2023-03-13 16:29:32 -0700

    move ValidSchema from Schema to Instance
        b/c we're going to put methods on it
        HI TWISOL

--- src/Mog/Instance.hs
+++ src/Mog/Instance.hs
@@ -1,17 +1,22 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE DataKinds #-}
+-- extensions for the validation classes
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 -- | Module for instances of a schema
 module Mog.Instance
     ( (:&)(..), TTablesEnd(..)
     , (:%)(..), Ø_(..)
     , Inst
+    , ValidSchema(..)
     ) where
 
 import Data.Kind (Type)
 import Data.Map (Map)
 import Data.Type.Equality ((:~:)(..))
+import Data.Proxy (Proxy(..))
 
 import Mog.Schema
 
@@ -79,3 +84,38 @@ _testInst13 = Refl
 _testInst20 :: Inst (Table "teble" (Prim Int % Ø ↦ Prim String % Ø))
            :~: Map (Int :% Ø_) (String :% Ø_)
 _testInst20 = Refl
+
+
+
+
+-- * Schema validation
+
+class                          ValidSchema a                    where prune :: Proxy a -> Inst a -> Inst a
+instance ValidTables tables => ValidSchema (Schema name tables) where
+
+class                                         ValidTables ts
+instance (ValidTable t ts, ValidTables ts) => ValidTables (t & ts)
+instance                                      ValidTables TablesEnd
+
+class                                           ValidTable t                 ts
+instance ValidTable pk_v ts                  => ValidTable (Table name pk_v) ts
+instance (ValidCols pk   ts, ValidCols v ts) => ValidTable (pk ↦ v)          ts
+
+class                                          ValidCols pk_v     ts
+instance (ValidCol   c ts, ValidCols cs ts) => ValidCols (c % cs) ts
+instance                                       ValidCols Ø        ts
+
+class                        ValidCol c              ts
+instance                     ValidCol (Prim a)       ts
+instance IsFk index fk ts => ValidCol (Ref fk index) ts
+
+-- ** Foreign keys
+
+-- | A fk must match the pk of the table indicated by the index.
+class                        IsFk (ix :: Index) k tables
+instance IsFk ix k tables => IsFk ('There ix)   k (t & tables)
+instance IsPk    k t      => IsFk  'Here        k (t & tables)
+
+class                   IsPk k t
+instance IsPk k pk_v => IsPk k (Table name pk_v)
+instance                IsPk k (k ↦ v)
--- src/Mog/Schema.hs
+++ src/Mog/Schema.hs
@@ -1,9 +1,6 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE TypeOperators #-}
--- extensions for the validation classes
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE FlexibleInstances #-}
 module Mog.Schema (
   type Schema,
   type (&),
@@ -19,8 +16,6 @@ module Mog.Schema (
   type Prim,
 
   Index(..),
-
-  ValidSchema,
 ) where
 
 import Data.Type.Equality ((:~:)(..))
@@ -60,38 +55,3 @@ data Prim a
 _testPrec10 :: Int & Char % Word % () ↦ String % Float % () & Double
            :~: Int & (((Char % (Word % ())) ↦ (String % (Float % ()))) & Double)
 _testPrec10 = Refl
-
-
-
-
--- * Schema validation
-
-class                          ValidSchema a
-instance ValidTables tables => ValidSchema (Schema name tables)
-
-class                                         ValidTables ts
-instance (ValidTable t ts, ValidTables ts) => ValidTables (t & ts)
-instance                                      ValidTables TablesEnd
-
-class                                           ValidTable t                 ts
-instance ValidTable pk_v ts                  => ValidTable (Table name pk_v) ts
-instance (ValidCols pk   ts, ValidCols v ts) => ValidTable (pk ↦ v)          ts
-
-class                                          ValidCols pk_v     ts
-instance (ValidCol   c ts, ValidCols cs ts) => ValidCols (c % cs) ts
-instance                                       ValidCols Ø        ts
-
-class                        ValidCol c              ts
-instance                     ValidCol (Prim a)       ts
-instance IsFk index fk ts => ValidCol (Ref fk index) ts
-
--- ** Foreign keys
-
--- | A fk must match the pk of the table indicated by the index.
-class                        IsFk (ix :: Index) k tables
-instance IsFk ix k tables => IsFk ('There ix)   k (t & tables)
-instance IsPk    k t      => IsFk  'Here        k (t & tables)
-
-class                   IsPk k t
-instance IsPk k pk_v => IsPk k (Table name pk_v)
-instance                IsPk k (k ↦ v)
