64c330da7bdf38069da0ca3b6a06d6af37605f51
PLR <51248199+plredmond@users.noreply.github.com>
2023-04-17 21:36:28 -0700

    prototype a UI class for domain-specific three way merge with @twisol

--- src/Mog.hs
+++ src/Mog.hs
@@ -16,4 +16,11 @@ class {-Relations (Abstracted a) =>-} MRDT a where
     type Abstracted a :: Type
     α :: a -> Abstracted a
     γ :: Abstracted a -> a
-    -- three-way merge
+
+class Mergeable a where
+    -- | @merge base ours theirs@ is a family of binary merges against a
+    -- base. Any @merge base@ is a commutative semigroup for
+    -- versions that have @base@ as an ancestor.
+    --
+    -- (TODO: It might be a monoid but we'll think about the identity later.)
+    merge :: a -> (a -> a -> a)
