484bbb4e669c2b59fdf7213ced99b0281fc056da
PLR <51248199+plredmond@users.noreply.github.com>
2023-04-21 20:22:58 -0700

    simplify Mog.hs

--- src/Mog.hs
+++ src/Mog.hs
@@ -1,22 +1,26 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeFamilies #-}
 
 -- | Module for UI/UX reexports.
 module Mog
+    -- * The MRDT typeclass
     ( MRDT(..)
+    -- ** Constraints for constituent types
+    , MaybeTuple
+    , Mergeable(..)
+    -- ** Grammar of Relations
     , Dt(Dt)
     , (::::)(Rel)
     , (:>)
     , (:@)(Ref)
-    , Mergeable(..)
-    , MaybeTuple
     ) where
 
 import Data.Kind (Type)
-import Mog.UI
+
 import Mog.MergeDriver.Merge (Mergeable(..))
+import Mog.UI
 
 class (DtC (Abstracted a)) => MRDT a where
     type Abstracted a :: Type
     α :: a -> Abstracted a
     γ :: Abstracted a -> a
-
