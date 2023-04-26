a9b670d489f57f128d5eb34bb57f62f7d794601a
PLR <51248199+plredmond@users.noreply.github.com>
2023-04-26 19:09:23 -0400

    move nixpkgs pin from shell.nix to default.nix

--- shell.nix
+++ shell.nix
@@ -1,8 +1,1 @@
-import ./. rec {
-  shell = true;
-  pkgs = import (builtins.fetchTarball {
-      # latest https://github.com/NixOS/nixpkgs/commits/nixos-22.11 as of Wed 26 Apr 2023 06:32:17 PM EDT
-      url = "https://github.com/NixOS/nixpkgs/archive/60c0f762658916a4a5b5a36b3e06486f8301daf4.tar.gz";
-      sha256 = "0z6jy05yawj2pkinpyjlpym5niij2scxn6cd2w1p9wrfxw0hw8ra";
-    }) {};
-}
+import ./. rec { shell = true; }
